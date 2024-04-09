dfc6d95e68a00fb23acf891a9d94a86825775fda
pjc0179 <pjc0179@gmail.com>
2024-04-09 22:57:22 +0900

    chore:"간단한 주석 제거 및 use client 추가 "

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.externals = [...config.externals, { canvas: 'canvas' }]; // required to make Konva & react-konva work
+    config.externals = [...config.externals, { canvas: 'canvas' }];
     return config;
   },
 };
