b5d3c99c089dde165463705c2ed7abbc8a225b9d
pjc0179 <pjc0179@gmail.com>
2024-04-11 21:20:00 +0900

    feat:" socket io와 연결하여 실시간 그리기 구현"

--- src/components/paint/Paint.tsx
+++ src/components/paint/Paint.tsx
@@ -3,6 +3,8 @@ import { KonvaEventObject } from 'konva/lib/Node';
 import { useRef, useState } from 'react';
 import { Stage, Layer, Text, Line } from 'react-konva';
 import { useEffect } from 'react';
+import { Socket, io } from 'socket.io-client';
+import { throttle } from 'lodash';
 
 interface ILine {
   tool: string;
@@ -14,6 +16,15 @@ interface History {
   lines: Array<ILine>;
 }
 
+interface EmitData {
+  type: 'painting' | 'startPaint' | 'undo' | 'redo';
+  tool: string;
+  position: {
+    x: number;
+    y: number;
+  };
+}
+
 const INIT_HISTORY: History = {
   currentIdx: 0,
   lines: [],
@@ -22,26 +33,33 @@ const INIT_HISTORY: History = {
 const Paint = () => {
   const [tool, setTool] = useState('pen');
   const [history, setHistory] = useState<History>(INIT_HISTORY);
+  const [socket, setSocket] = useState<Socket>();
+  const [isConnected, setIsConnected] = useState<boolean>(false);
 
   const lines = history.lines.slice(0, history.currentIdx);
   const isPainting = useRef(false);
-
+  console.log(history);
   const handleStartPaint = (e: KonvaEventObject<MouseEvent>) => {
     isPainting.current = true;
 
-    const pos = e.target.getStage()?.getPointerPosition();
-    if (!pos) return;
-
-    const lines = history.lines.slice(0, history.currentIdx);
-
-    setHistory({
-      ...history,
-      currentIdx: history.currentIdx + 1,
-      lines: [...lines, { tool, points: [pos.x, pos.y] }],
+    const point = e.target.getStage()?.getPointerPosition();
+    if (!point) return;
+    // setHistory({
+    //   ...history,
+    //   currentIdx: history.currentIdx + 1,
+    //   lines: [...lines, { tool, points: [point.x, point.y, point.x, point.y] }],
+    // });
+    emitMessage({
+      type: 'startPaint',
+      tool,
+      position: {
+        x: point.x,
+        y: point.y,
+      },
     });
   };
 
-  const handlePainting = (e: KonvaEventObject<MouseEvent>) => {
+  const throttledHandlePainting = throttle((e: KonvaEventObject<MouseEvent>) => {
     if (!isPainting.current) {
       return;
     }
@@ -49,12 +67,28 @@ const Paint = () => {
     const point = stage?.getPointerPosition();
     if (!point) return;
 
-    const lastLine = history.lines[history.lines.length - 1];
-    lastLine.points = [...lastLine.points, point.x, point.y];
+    // const lastLine = history.lines[history.lines.length - 1];
+    // lastLine.points = [...lastLine.points, point.x, point.y];
 
-    const newLines = [...history.lines];
-    newLines[newLines.length - 1] = { ...newLines[newLines.length - 1] };
-    setHistory({ ...history, lines: [...newLines] });
+    // const newLines = [...history.lines];
+    // newLines[newLines.length - 1] = { ...newLines[newLines.length - 1] };
+    // setHistory((history) => ({
+    //   ...history,
+    //   lines: [...newLines]
+    // }));
+
+    emitMessage({
+      type: 'painting',
+      tool,
+      position: {
+        x: point.x,
+        y: point.y,
+      },
+    });
+  }, 200); // 100밀리초 간격으로 스로틀링
+
+  const handlePainting = (e: KonvaEventObject<MouseEvent>) => {
+    throttledHandlePainting(e);
   };
 
   const handleStopPaint = () => {
@@ -75,6 +109,15 @@ const Paint = () => {
     });
   };
 
+  const emitMessage = async (message: EmitData) => {
+    await fetch('/api/chat', {
+      method: 'POST',
+      body: JSON.stringify({
+        ...message,
+      }),
+    });
+  };
+
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       const key = e.key.toLowerCase();
@@ -90,7 +133,65 @@ const Paint = () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
+  useEffect(() => {
+    const initSocket = () => {
+      const socket = io('http://localhost:3000', {
+        path: '/api/socket/io',
+        addTrailingSlash: false,
+      });
+      socket.on('connect', () => {
+        setIsConnected(true);
+      });
 
+      socket.on('message', (res) => {
+        const data = JSON.parse(res) as EmitData;
+
+        if (data.type === 'startPaint') {
+          setHistory((history) => {
+            const lines = history.lines.slice(0, history.currentIdx);
+            const newHistory = {
+              ...history,
+              currentIdx: history.currentIdx + 1,
+              lines: [
+                ...lines,
+                {
+                  tool: data.tool,
+                  points: [
+                    data.position.x,
+                    data.position.y,
+                    data.position.x,
+                    data.position.y,
+                  ],
+                },
+              ],
+            };
+            return newHistory;
+          });
+        }
+        if (data.type === 'painting') {
+          setHistory((history) => {
+            const lastLine = history.lines[history.lines.length - 1];
+            lastLine.points = [...lastLine.points, data.position.x, data.position.y];
+
+            const newLines = [...history.lines];
+            newLines[newLines.length - 1] = { ...newLines[newLines.length - 1] };
+            return { ...history, lines: [...newLines] };
+          });
+        }
+      });
+
+      setSocket(socket);
+    };
+    initSocket();
+    return () => {
+      if (socket) {
+        socket.disconnect();
+      }
+    };
+  }, []);
+  if (!isConnected) {
+    return <div>로딩중입니다</div>;
+  }
   return (
     <div>
       <div className="flex">
